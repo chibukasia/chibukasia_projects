8e17c35c794b018f3ac5101975dffa44c9401412
unknown <chibukasianelson@gmail.com>
2019-01-13 12:19:26 +0300

    python files changes

---
+++ jzoo.py
@@ -0,0 +1,17 @@
+print("hello, what is your name?")
+myname=input()
+print("my name is " + myname)
+print("it is good to know you " + myname)
+print("the length of your name is")
+print(len(myname))
+print("okay, let's go to your privacy")
+print("enter your password")
+password=input()
+if password=='hello':
+    print("access granted")
+else:
+    print("hello stranger")
+print("how old are you??")
+age=input()
+print("you will be  "   + str(int(age)  +  1 ) +  " in a a year " + myname)
+
